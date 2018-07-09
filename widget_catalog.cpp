8e32913c66213cb874acf4fa90376fb229af2d7e
WeSeewy <1509670254@qq.com>
2018-07-09 17:06:13 +0800

    QTreeWidget

--- widget_catalog.cpp
+++ widget_catalog.cpp
@@ -6,11 +6,45 @@ widget_catalog::widget_catalog(QWidget *parent) :
     ui(new Ui::widget_catalog)
 {
     ui->setupUi(this);
+    this->setWindowTitle(u8"文件资源查看");
     ui->table_file->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
     ui->table_file->horizontalHeader()->setSectionResizeMode(0,QHeaderView::ResizeToContents);
+//    timer.setInterval(8000);
+//    connect(&timer,SIGNAL(timeout()),this,SLOT(init()));
+//    if(!timer.isActive())   timer.start();
 }
 
 widget_catalog::~widget_catalog()
 {
     delete ui;
+}
+
+void widget_catalog::init(){
+    ui->treeWidget->clear();
+    ui->treeWidget->addTopLevelItem(proot);//添加顶级项root
+//    proot->addChild(pbin);
+//    proot->addChild(pusr);
+//    proot->addChild(pdev);
+//    addchildren(pbin);
+//    addchildren(pusr);
+//    addchildren(pdev);
+    addchildren(proot,ROOT);
+}
+
+void widget_catalog::addchildren(QTreeWidgetItem *faItem,dir*faDir ){
+    for(int i=2;i<faDir->nsub;i++){
+        if(INODE[faDir->num[i]].type==1){
+            faItem->addChild(new QTreeWidgetItem(QStringList()<< QString::fromStdString(faDir->name[i])));
+        }
+        else if(INODE[faDir->num[i]].type==2){
+            QTreeWidgetItem *newitem=new QTreeWidgetItem(QStringList()<< QString::fromStdString(faDir->name[i]));
+            faItem->addChild(newitem);
+            addchildren(newitem,INODE[faDir->num[i]].pdir);
+        }
+    }
+}
+
+void widget_catalog::on_pushButton_3_clicked()
+{
+    init();
 }
--- widget_catalog.h
+++ widget_catalog.h
@@ -2,7 +2,11 @@
 #define WIDGET_CATALOG_H
 
 #include <QWidget>
-
+#include <QString>
+#include <QStringList>
+#include <QTimer>
+#include <QTreeWidgetItem>
+#include "file.h"
 namespace Ui {
 class widget_catalog;
 }
@@ -14,9 +18,20 @@ class widget_catalog : public QWidget
 public:
     explicit widget_catalog(QWidget *parent = 0);
     ~widget_catalog();
+//    QTimer timer;
+
+private slots:
+    void init();
+
+    void on_pushButton_3_clicked();
 
 private:
     Ui::widget_catalog *ui;
+    void addchildren(QTreeWidgetItem *faItem,dir* faDir);
+    QTreeWidgetItem *proot=new QTreeWidgetItem(QStringList()<< QString::fromStdString("root"));
+//    QTreeWidgetItem *pbin=new QTreeWidgetItem("bin");
+//    QTreeWidgetItem *pusr=new QTreeWidgetItem("usr");
+//    QTreeWidgetItem *pdev=new QTreeWidgetItem("dev");
 };
 
 #endif // WIDGET_CATALOG_H
