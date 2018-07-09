6b3174ba934392c691fc5947b5133e204320afc5
puzzledsky <puzzledsky@hotmail.com>
2018-07-09 15:15:35 +0800

    dir 改到cpp里

--- file.h
+++ file.h
@@ -33,10 +33,47 @@ Users::loginOut("1");
 更新时间 
 */
 
-class dir;
 class inode;
 class user;
 extern int UID;
+class dir {
+private:
+	int getFree();//获得空闲节点    
+	static dir* getDir(int n);//inode号为n对应的目录
+	dir* getParent();//返回父亲节点指针
+	int find(string s);
+	bool haveRight(string user, string name, int method);
+	void update();
+public:
+	int num[100];
+	string name[100];
+	int nsub;
+
+	dir(string s);
+
+	dir* addDir(string s);//在当前目录下添加目录x
+	void remove();
+	bool rename(string s);
+	void print();
+	dir* in(string s);
+	dir* out();
+
+	int findFile(string s);
+	inode* getFile(string s);
+	bool addFile(string s);
+	void setUser(string s, string user);
+	void setRight(string s, int right);
+	int getRight(string s);
+	int openFile(string s, string user, int method);
+	void closeFlie(string s, int method);
+
+	bool removeFile(string s);
+	bool renameFile(string s, string name1);
+	string readFile(string s);
+	void writeFile(string s, string str);
+
+	string getName();
+};
 class Users {
 public:
 	static int findUser(string n);
@@ -210,6 +247,18 @@ public:
 		size = s.length();
 		write();
 	}
+	void setDirData() {
+		if (type != 2)
+			return;
+		string data = "";
+		for (int i = 0; i < pdir->nsub; i++) {
+			data.append(to_string(pdir->num[i]));
+			data.append(" ");
+			data.append(pdir->name[i]);
+			data.append("\n");
+		}
+		setData(data);
+	}
 	void printData() {
 		for (int i = 0; i < 8; i++) {
 			if (addr[i] == -1)
@@ -240,255 +289,7 @@ public:
 };
 extern inode INODE[BLOCKTOI * ISIZE+5];
 
-class dir {
-private:
-    //获得空闲节点
-	int getFree() {
-		if (nsub == 100)
-			return -1;
-		int p = 2;
-		while (p<nsub)
-		{
-			if (num[p] == 0)
-				break;
-			p++;
-		}
-		if (p == nsub)
-			nsub++;
-		return p;
-	}
 
-    //inode号为n对应的目录
-	static dir* getDir(int n) {
-		return INODE[n].pdir;
-	}
-
-    //返回父亲节点指针
-	dir* getParent() {
-		return getDir(num[0]);
-	}
-	int find(string s) {//查找文件或者目录，返回在目录表中的下标
-		REM.push(num[1]);
-		for (int i = 0; i < nsub; i++) {
-			if (num[i] && name[i] == s) {
-				return i;
-			}
-		}
-		return -1;
-	}
-	bool haveRight(string user,string name,int method) { //method 1：读  2：写  3:执行
-		if (user == "root")
-			return true;
-		int id = Users::getUid(user);
-		int p = find(name);
-		if (INODE[num[p]].uid == id) {
-			return INODE[num[p]].getRight(10-method);
-		}
-		//组的放这里
-		if (INODE[num[p]].gid == Users::getGid(user))
-			return INODE[num[p]].getRight(7 - method);
-
-		return INODE[num[p]].getRight(4-method);
-	}
-public:
-	int num[100];
-	string name[100];
-	int nsub;
-
-	dir(string s) {
-		nsub = 2;
-		name[0] = "NULL";
-		num[0] = 0;
-		name[1] = s;
-		int di = sblock.i_get();
-		num[1] = di;
-		INODE[di].pdir = this;
-		INODE[di].type = 2;
-	}
-	
-
-    //在当前目录下添加目录x
-	dir* addDir(string s) {//s:目录名
-		
-		if (this->find(s) != -1) {
-			cerr << "命名冲突" << endl;
-			return NULL;
-		}
-		dir* x = newDir(s);
-
-		int p = getFree();
-		name[p] = x->name[1];
-		num[p] = x->num[1];
-		x->num[0] = num[1];
-		x->name[0] = name[1];
-		return x;
-	}
-	void remove() {//删除目录自己，包括子目录和文件
-		for (int i = 2; i < nsub; i++) {
-			if (INODE[num[i]].type == 1) {
-				sblock.i_put(num[i]);
-			}
-			else if (INODE[num[i]].type == 2) {
-				getDir(num[i])->remove();
-			}
-		}
-		if (num[0] != 0) {
-			dir *p = getParent();
-			int x = p->find(name[1]);
-			p->num[x] = 0;
-		}
-		sblock.i_put(num[1]);
-		this->~dir();
-	}
-	bool rename(string s) {//目录重命名
-		if (s == name[1])
-			return true;
-		dir* p = getParent();
-		int x1 = p->find(s);
-		if (x1 == -1) {
-			int x = p->find(name[1]);
-			name[1] = s;
-			p->name[x] = s;
-			for (int i = 2; i < nsub; i++) {
-				if (num[i]) {
-					getDir(num[i])->name[0] = s;
-				}
-			}
-			return true;
-		}
-		return false;
-	}
-	void print() {//输出目录表
-		for (int i = 0; i < nsub; i++) {
-			cout << name[i] << "  " << num[i] << endl;
-		}
-		cout << endl;
-	}
-	dir* in(string s) {//进入子目录，失败时留在当前目录
-		int p = find(s);
-		if (p != -1 && INODE[num[p]].type == 2) {
-			REM.push(num[p]);
-			return INODE[num[p]].pdir;
-		}
-		return this;
-	}
-	dir* out() {//返回上一级目录，失败时留在当前目录
-		if (num[0] == 0)
-			return this;
-		REM.pop(num[0]);
-		return INODE[num[0]].pdir;
-	}
-	
-	
-	int findFile(string s) {//根据文件名找文件，没找到则返回-1
-		int p = find(s);     
-        if (p!=-1 && INODE[num[p]].type != 1) {
-			p = -1;
-		}
-		if (p != -1)
-			REM.push(num[p]);
-		return p;
-	}
-	inode* getFile(string s) {//在当前目录查找指定文件，返回inode，失败时返回NULL
-		int p = findFile(s);
-		if (p == -1)
-			return NULL;
-		return &INODE[p];
-	}
-	bool addFile(string s) {//s：文件名，添加文件
-		if (find(s) != -1)// 命名冲突
-			return false;
-		int di = sblock.i_get();
-		INODE[di].toFile();
-		int p = getFree();
-		name[p] = s;
-		num[p] = di;
-		return true;
-	}
-	void setUser(string s, string user) {
-		int p = find(s);
-		if (p == -1)
-			return;
-		int uid =Users::getUid(user);
-		int gid = Users::getGid(user);
-		INODE[num[p]].uid = uid;
-		INODE[num[p]].gid = gid;
-	}
-	void setRight(string s, int right) {
-		int p = find(s);
-		if (p == -1)
-			return;
-		INODE[num[p]].setRight(right);
-	}
-	int getRight(string s) {//返回0时查找失败
-		int p = find(s);
-		if (p == -1)
-			return 0;
-		return INODE[num[p]].right;
-	}
-	/*权限还没写*/
-	// ->openFile("1.txt","user",1)
-	int openFile(string s, string user,int method) {//返回值  -1:不存在 -2:无权限 0:被占用  1:成功
-		int p = findFile(s);	                    //method 1：读  2：写   3:执行
-        cout<<"p--index:"<<p<<endl;
-        if (p == -1)
-			return -1;
-		if (!haveRight(user, s, method))
-			return -2;
-		if (INODE[num[p]].nwrite == 1)
-			return 0;
-		if (method == 1) {
-			INODE[num[p]].nread++;
-			return 1;
-		}
-		if (method == 2 && INODE[num[p]].nread==0) {
-			INODE[num[p]].nwrite = 1;
-			return 1;
-		}
-		return 0;
-	}
-	//->closeFile("1.txt",1)
-	void closeFlie(string s,int method) {//method 1：读  2：写
-		int p = findFile(s);            
-		if (p == -1)
-			return;
-		if (method == 1) {
-			if(INODE[num[p]].nread>0)
-				INODE[num[p]].nread--;
-		}
-		if (method == 2) {
-			INODE[num[p]].nwrite = 0;
-		}
-	}
-	
-	bool removeFile(string s) {//s：文件名，删除文件
-		int p = findFile(s);
-		if (p == -1)
-			return false;
-		sblock.i_put(num[p]);
-		num[p] = 0;
-		return true;
-	}
-	bool renameFile(string s,string name1) {//s:文件名，name1：新文件名， 文件重命名
-		int p = findFile(s);
-		int p1 = find(name1);
-		if (p1 != -1)
-			return false;
-		name[p] = name1;
-	}
-	string readFile(string s) {//  返回文件全部内容
-		int p = findFile(s);
-		return INODE[num[p]].getData();
-	}
-	void writeFile(string s,string str) {//s：文件名，str：新的内容（全部） 重写文件 
-		int p = findFile(s);
-		INODE[num[p]].setData(str);
-	}
-
-    string getName(){
-        return name[1];
-    }
-};
 
 class user {
 public:
